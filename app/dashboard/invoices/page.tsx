089f9e07548ba3cde2088a17d006daabb0de1e1c
Senitdev <75537054+Senitdev@users.noreply.github.com>
2025-04-15 20:34:50 +0000

    FC du 15

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,7 +4,6 @@ import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
 export default function Invoices() {
     return (
         <div className="w-full">
